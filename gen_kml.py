e712b71279692bdfca53add04af03720cb13836d
Binux <17175297.hk@gmail.com>
2013-02-02 00:47:58 +0800

    img style

--- gen_kml.py
+++ gen_kml.py
@@ -131,11 +131,11 @@ def build_kml(city):
         name = portal.title
         latlng = portal.latlng
         desc = '<br />'.join([
-                    'Level: %s' % portal.level,
-                    'Resonators: %d/8 | Mods: %d/4 | Energy: %d (%0.f%%)' % (len([x for x in portal.resonators if x]), len([x for x in portal.mods if x]), portal.energy, portal.energy / (portal.total_energy + 0.00000001) * 100.0),
+                    'Level: %s | Energy: %d (%0.f%%)' % (portal.level, portal.energy, portal.energy / (portal.total_energy + 0.00000001) * 100.0),
+                    'Resonators: %d/8 | Mods: %d/4' % (len([x for x in portal.resonators if x]), len([x for x in portal.mods if x])),
                     ('Map: <a href="https://maps.google.com/maps?q=%s%%40%s,%s" target="_blank">Google</a>' % (quote(name.encode('utf8')), latlng.lat, latlng.lng))
                     +(' <a href="http://www.ingress.com/intel?latE6=%d&lngE6=%d&z=17" target="_blank">Ingress</a>' % (latlng.lat*1e6, latlng.lng*1e6)),
-                    '<img src="%s" />' % portal.image,])
+                    '<img src="%s" style="max-width: 300px; max-height: 300px;" />' % portal.image,])
 
         pnt = kml.newpoint(name=portal.title, description=desc, coords = [(latlng.lng, latlng.lat),])
         pnt_fixed = kml_fixed.newpoint(name=portal.title, description=desc, coords = [gps2gmap(latlng.lat, latlng.lng)[::-1],])
